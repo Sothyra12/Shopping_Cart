fc92bd3b66e996428bf5160720a020591ba2501e
Sothyra Chan <schan256@my.centennialcollege.ca>
2025-01-12 21:20:52 -0500

    get element by ids, created array of objects of the products, displayed all the products using loop and declared class

--- script.js
+++ script.js
@@ -1,1 +1,109 @@
 // script.js
+const cartContainer = document.getElementById("cart-container");
+const productsContainer = document.getElementById("products-container");
+const dessertCards = document.getElementById("dessert-card-container");
+const cartBtn = document.getElementById("cart-btn");
+const clearCartBtn = document.getElementById("clear-cart-btn");
+const totalNumberOfItems = document.getElementById("total-items");
+const cartSubTotal = document.getElementById("subtotal");
+const cartTaxes = document.getElementById("taxes");
+const cartTotal = document.getElementById("total");
+const showHideCartSpan = document.getElementById("show-hide-cart");
+let isCartShowing = false;
+
+const products = [
+  {
+    id: 1,
+    name: "Vanilla Cupcakes (6 Pack)",
+    price: 12.99,
+    category: "Cupcake",
+  },
+  {
+    id: 2,
+    name: "French Macaron",
+    price: 3.99,
+    category: "Macaron",
+  },
+  {
+    id: 3,
+    name: "Pumpkin Cupcake",
+    price: 3.99,
+    category: "Cupcake",
+  },
+  {
+    id: 4,
+    name: "Chocolate Cupcake",
+    price: 5.99,
+    category: "Cupcake",
+  },
+  {
+    id: 5,
+    name: "Chocolate Pretzels (4 Pack)",
+    price: 10.99,
+    category: "Pretzel",
+  },
+  {
+    id: 6,
+    name: "Strawberry Ice Cream",
+    price: 2.99,
+    category: "Ice Cream",
+  },
+  {
+    id: 7,
+    name: "Chocolate Macarons (4 Pack)",
+    price: 9.99,
+    category: "Macaron",
+  },
+  {
+    id: 8,
+    name: "Strawberry Pretzel",
+    price: 4.99,
+    category: "Pretzel",
+  },
+  {
+    id: 9,
+    name: "Butter Pecan Ice Cream",
+    price: 2.99,
+    category: "Ice Cream",
+  },
+  {
+    id: 10,
+    name: "Rocky Road Ice Cream",
+    price: 2.99,
+    category: "Ice Cream",
+  },
+  {
+    id: 11,
+    name: "Vanilla Macarons (5 Pack)",
+    price: 11.99,
+    category: "Macaron",
+  },
+  {
+    id: 12,
+    name: "Lemon Cupcakes (4 Pack)",
+    price: 12.99,
+    category: "Cupcake",
+  },
+];
+
+// Display all products in the html using forEach
+// destructuring the object to get the name, id, price, and category
+// used += to append the html to the dessertCards div to make sure all products are displayed and not replaced with the new product's content when it is added
+products.forEach(({ name, id, price, category }) => {
+  dessertCards.innerHTML += `
+        <div class="dessert-card">
+          <h2>${name}</h2>
+          <p class="dessert-price">$${price}</p>
+          <p class="product-category">Category: ${category}</p>
+          <button 
+            id="${id}" 
+            class="btn add-to-cart-btn">Add to cart
+          </button>
+        </div>
+      `;
+});
+
+// a class is like a blueprint for creating objects. It allows you to define a set of properties and methods, and instantiate (or create) new objects with those properties and methods.
+class ShoppingCart {
+
+};
